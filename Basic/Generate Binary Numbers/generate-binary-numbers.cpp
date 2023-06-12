7d3365bd85f2fff0fba3603adf6fe5bcbb8301ee
smritisrivastav20 <47885989+smritisrivastav20@users.noreply.github.com>
2023-06-12 13:39:44 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Generate Binary Numbers/generate-binary-numbers.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+
+//Function to generate binary numbers from 1 to N using a queue.
+vector<string> generate(int N)
+{
+	// Your code here
+	queue<string>q;
+	vector<string>s;
+	q.push("1");
+	for(int count=0;count<N;count++)
+	{
+	    s.push_back(q.front());
+	   
+	    q.push(q.front()+"0");
+	    q.push(q.front()+"1");
+	     q.pop();
+	}
+	return s;
+}
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		vector<string> ans = generate(n);
+		for(auto it:ans) cout<<it<<" ";
+		cout<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
